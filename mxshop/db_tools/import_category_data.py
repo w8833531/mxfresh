dcce0dbb941dea7f61dd3c7ed834b3ae70933a6d
w8833531 <w8833531@hotmail.com>
2018-03-22 13:40:49 +0800

    导入商品类别数据 注： 原文中sys.path.append(pwd+'../')无法成功append path， 会报错，改成sys.path.insert

--- mxshop/db_tools/import_category_data.py
+++ mxshop/db_tools/import_category_data.py
@@ -7,8 +7,10 @@ import os
 
 
 pwd = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(pwd+"../")
+# sys.path.append(pwd+"../")
+sys.path.insert(0, os.path.join(pwd, '../'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mxshop.settings")
+print(os.environ.items())
 
 import django
 django.setup()
