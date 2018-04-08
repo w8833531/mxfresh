27bef31d45783403e51aefc22c0351e9b813e8a7
w8833531 <w8833531@hotmail.com>
2018-04-08 22:47:06 +0800

    delete import django-notenv in settings

--- mxshop/mxshop/settings.py
+++ mxshop/mxshop/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import sys
 import datetime
-from dotenv import parse_dotenv, read_dotenv
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
